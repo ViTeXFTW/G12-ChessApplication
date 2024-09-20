b2ee074675f22914ec143723ac067e44e0e2602c
Jacob <s215528@dtu.dk>
2024-09-20 22:23:36 +0200

    feat: Added rudimentary Login screen.
    Added initial login screen set up, along with textboxes for user-input and buttons. Remember to change the startup screen in app.xaml to change the initial start up screen.

---
+++ LoginScreen.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_screen
+{
+    public partial class LoginScreen : Window
+    {
+        private string userName = "";
+        private string passWord = "";
+
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInButton_Click(object sender, RoutedEventArgs e)
+        {
+            userName = userNameInput.Text;
+            passWord = passWordInput.Text;
+
+            if (userName == "Username" || passWord == "Password")
+            {
+                warningMessage.Content = "Please enter both a username and a password to proceed.";
+                return;
+            }
+
+
+            Console.WriteLine($"Hello {userName}");
+
+        }
+    }
+}
